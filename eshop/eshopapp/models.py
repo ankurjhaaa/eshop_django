3d25dc27da56b5a724e10671f21b068ce1a95e4f
ankurjhaaa <kumarankur7258@gmail.com>
2025-12-25 01:20:28 +0530

    pillow download , product model create

--- eshop/eshopapp/models.py
+++ eshop/eshopapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    parent_id = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.CharField(unique=True, max_length=200)
+    cat_id = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
+    mrp = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(null=True,blank=True)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
